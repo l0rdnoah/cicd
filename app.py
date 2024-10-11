8513e9b82cabfe8d07773b85364cdb9c973fc692
Noah Brohan <noah.brohan@epitech.eu>
2024-10-11 13:54:46 +0200

    test

---
+++ app.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Addition de 1 et 2 :", add(1, 2))
+    print("Soustraction de 5 et 3 :", subtract(5, 3))
---
+++ test.py
@@ -0,0 +1,18 @@
+from app import add, subtract
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
+    assert add(0, 0) == 0
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(10, 5) == 5
+    assert subtract(0, 0) == 0
+    assert subtract(-1, -1) == 0
+
+if __name__ == "__main__":
+    test_add()
+    test_subtract()
+    print("Tous les tests sont passés !")
